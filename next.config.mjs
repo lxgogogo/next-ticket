0c7fcab393ed63e7d8c12e3a122e6387384503b1
lxgogogo <lxgogogo@outlook.com>
2024-02-06 15:29:59 +0800

    env

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_ADDRESS: "http://localhost:3000/api",
-    // API_ADDRESS: "https://next-ticket-bay.vercel.app/api",
+    // API_ADDRESS: "http://localhost:3000/api",
+    API_ADDRESS: "https://next-ticket-bay.vercel.app/api",
   },
 };
 
